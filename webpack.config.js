577b7573cee334c76c9c3a0cb6680dfbe350e91e
Nunchakus Lei <jimmynunchakuslei@gmail.com>
2018-03-11 22:12:25 -0600

    able to load obj file 2

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ var vtkRules = require('vtk.js/Utilities/config/dependency.js').webpack.v2.rules
 var entry = path.join(__dirname, './app/static/app/main.js');
 const sourcePath = path.join(__dirname, './app/static/app/scripts');
 const outputPath = path.join(__dirname, './app/static/app/scripts/build');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   entry,
@@ -15,7 +16,50 @@ module.exports = {
   module: {
     rules: [
         { test: entry, loader: "expose-loader?build" },
+        {
+          test: /\.js$/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['env'],
+                // presets: [['env', { targets: { browsers: 'last 2 versions' } }]],
+              },
+            },
+          ],
+        },
+        { test: /\.(png|jpg)$/, use: 'url-loader?limit=81920' },
+
         { test: /\.html$/, loader: 'html-loader' },
+        {
+          test: /\.mcss$/,
+          use: [
+            { loader: 'style-loader' },
+            {
+              loader: 'css-loader',
+              options: {
+                localIdentName: '[name]-[local]_[sha512:hash:base32:5]',
+                modules: true,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => [autoprefixer('last 2 version', 'ie >= 10')],
+              },
+            },
+          ],
+        },
+        {
+          test: /\.svg$/,
+          use: [{ loader: 'raw-loader' }],
+        },
+        {
+          test: /\.worker\.js$/,
+          use: [
+            { loader: 'worker-loader', options: { inline: true, fallback: false } },
+          ],
+        },
     ].concat(vtkRules),
   },
   resolve: {
